d4f2568b96aac7977038da4a7610a8f3d0ff6c4f
unknown <keegood8@gmail.com>
2019-12-06 20:58:02 -0800

    Tone JS playing a series of chords from an array.

--- toneAndMidi/toneAndMidi.js
+++ toneAndMidi/toneAndMidi.js
@@ -13,25 +13,31 @@ function main(){
         "portamento" : 0.005
     }).toMaster()
 
-    // let piano = new Tone.PolySynth(4, Tone.Synth()).toMaster();
-
     let chords = [['C4','E4','G4','B4'], ['F4','A4','C5','E5'], ['G4','B4','D4','F5']]
 
     let count = 0;
     
-    Tone.Transport.scheduleRepeat(time => {
-        playChord(time, chords[count % chords.length]);//, chords[count % chords.length]);
-        // playBeat(time);
-        count++;
-    },'1m');
+    // Tone.Transport.scheduleRepeat(time => {
+    //     playChord(time, chords[count % chords.length]); // count up until the end of chords array, then repeat
+    //     count++;
+    // },'1m');
 
-    const playChord = (time, chord) => {
-        piano.triggerAttackRelease(chord, '2n', time);
+    let part = new Tone.Part((time, value) => {
+        playChord(time, value);
+    },
+    [{"time" : 0, "note" : ['C4','E4','G4','B4'], "velocity": 0.9, 'duration':'1m'}, 
+     {"time" : "1:0", "note" : ['F4','A4','C5','E5'], "velocity": 0.5, 'duraiton':'1m'}])
+    .start(0);
+    
+    const playChord = (time, value) => {
+        piano.triggerAttackRelease(value.note, value.duration, time);
     }
 
     Tone.Transport.start();
 
-
+    setTimeout(() => {
+        Tone.Transport.stop();
+    }, 6000);
 }
 
 main();
