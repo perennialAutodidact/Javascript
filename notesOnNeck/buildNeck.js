9c3d17ead20cdda85aae0ff76e67cfc0f5580f67
unknown <keegood8@gmail.com>
2019-12-08 19:14:20 -0800

    decided to refactor to use prototypes

--- notesOnNeck/buildNeck.js
+++ notesOnNeck/buildNeck.js
@@ -21,18 +21,19 @@ const getInstrumentStrings = (instrument, tuning) => {
 const drawNeck = (instrument, tuning, startFret, endFret) => {
     const neck = document.querySelector('#neck');
     const stringNames = getInstrumentStrings(instrument, tuning);
+    
     let stringName = '';
 
     // create strings
     for(let i=0; i<stringNames.length + 1; i++){
         stringName = stringNames[i];
-        
-        let string = drawString(stringName, stringNames.length, startFret, endFret);
+        stringId = i;
+        let string = drawString(stringId, stringName, stringNames.length, startFret, endFret);
         
         //create frets
         for(let j=startFret; j<endFret; j++){
             let fretNumber = j;
-            let fret = drawFret(stringName, fretNumber, string);
+            let fret = drawFret(stringName, stringNames.length, fretNumber, string);
 
             string.append(fret);
         }
@@ -48,42 +49,51 @@ const drawString = (stringName, totalStrings, startFret, endFret) => {
     let string = document.createElement('div');
     string.classList.add('string');
 
-    string.id = `${stringName}-string`;
+    string.setAttribute('name', `${stringName}-string`);
+    string.id = strings[
 
-    // console.log(strings);
-    
     if(strings.length == 0 || strings.length == totalStrings){
         string.classList.add('invisible-string');
+
+        if(strings.length == totalStrings){
+            string.setAttribute('name', 'invisible-string');
+        }
     }
 
     return string
 };
 
-const drawFret = (stringName, fretNumber, string) => {
+const drawFret = (stringName, totalStrings, fretNumber, string) => {
     let strings = document.querySelectorAll('.string');
     let fret = document.createElement('div');
     let fretNote = getFretNote(stringName, fretNumber);
 
-    fret.id = fretNote;
-    
+    fret.setAttribute('name', fretNote);
+
     if(fretNumber == 0){
         fret.classList.add('open-fret');
 
         if(string.classList.contains('invisible-string')){
             fret.classList.add('invisible-open-fret');
+            // fret.setAttribute('name','invisible-open-fret')
         }
     } else {
         fret.classList.add('fret');
-        if(string.classList.contains('invisible-string')){
+
+
+
+        if(string == strings[strings.length]){
+            console.log('true');
+            
             fret.classList.add('invisible-fret');
+            fret.setAttribute('name', 'invisible-fret');
         }
     }
-
-
     return fret
 };
 
 const getFretNote = (stringName, fretNumber) => {
+    // const notes = ['c', 'csharp-dflat', 'd', 'dsharp-eflat', 'e', 'f', 'fsharp-gflat', 'g', 'gsharp-aflat', 'a', 'asharp-bflat', 'b'];
     const notes = ['C', 'Csharp-Dflat', 'D', 'Dsharp-Eflat', 'E', 'F', 'Fsharp-Gflat', 'G', 'Gsharp-Aflat', 'A', 'Asharp-Bflat', 'B'];
 
     let startNote = notes.indexOf(stringName);
@@ -92,31 +102,26 @@ const getFretNote = (stringName, fretNumber) => {
 }
 
 const placeMarker = (string,fret) => {
-
     let targetFrets = document.querySelectorAll(`#${string} #${fret}`);
     
     for(let i=0; i<targetFrets.length; i++){
         let marker = document.createElement('div');
         marker.classList.add('note-marker');
-        console.log(targetFrets[i]);
         
         targetFrets[i].append(marker);
     }
 };
 
-const removeMarker = (string,fret) => {
+const removeMarker = (string='all',fret='all') => {
     let markers = document.querySelectorAll(`#${string} #${fret} > .note-marker`);
 
     for(let i=0; i<markers.length; i++){
-
         markers[i].remove();
     }
 };
 
-
-
 drawNeck('guitar', 'standard', 0, 13);
-placeMarker('E-string', 'Fsharp-Gflat');
-removeMarker('E-string', 'Fsharp-Gflat');
+// placeMarker('E-string', 'Fsharp-Gflat');
+// removeMarker('E-string', 'Fsharp-Gflat');
 
-placeMarker('E-string', 'E')
+// placeMarker('E-string', 'E')
--- notesOnNeck/scalesToNeck.js
+++ notesOnNeck/scalesToNeck.js
@@ -1,15 +1,37 @@
-const getScaleForumla = scaleName => {
-    const scales = {
-        'ionian': ['1','2','3','4','5','6','7'],
-        'dorian': ['1','2','b3','4','5','6','b7'],
-        'phrygian': ['1','b2','b3','4','5','b6','b7'],
-        'lydian': ['1','2','3','#4','5','6','7'],
-        'mixolydian': ['1','2','3','4','5','6','b7'],
-        'aeolian': ['1','2','b3','4','5','6','b7'],
-        'locrian': ['1','b2','b3','4','b5','b6','b7'],
-    }
+const applyScaleToNeck = (scaleName, key) => {
+    let tonic = teoria.note(key),
+        scale = tonic.scale(`${scaleName}`).simple(),
+        frets = document.querySelectorAll('.fret'),
+        openFrets = document.querySelectorAll('.open-fret'),
+        fretId,
+        openFretId,
+        indx,
+        note,
+        fretNote;
 
-    return scales[scaleName]
+        for(let i=0; i<frets.length; i++){
+            // console.log(`frets: ${frets[i].id}`);
+
+        // console.log(fretId);
+        
+    }
+    //
+
 }
-let a4 = teoria.note('a4');
-console.log(a4.scale('mixolydian').simple());
+
+const scaleFormulaInput = document.querySelector('#scale-formula-input');
+const keyInput = document.querySelector('#key-input')
+
+scaleFormulaInput.addEventListener('change', () => {
+    let tonic = keyInput.value;
+    let scaleName = scaleFormulaInput.value; 
+
+    applyScaleToNeck(scaleName, tonic);
+});
+
+keyInput.addEventListener('change', () => {
+    let tonic = keyInput.value;
+    let scaleName = scaleFormulaInput.value;
+
+    applyScaleToNeck(scaleName, tonic);
+});
