3c36ef59cb9e18258e2883cb9fd11ce78713da7a
unknown <keegood8@gmail.com>
2019-12-12 18:56:36 -0800

    add intToNote()

--- notesOnNeck/noteOps.js
+++ notesOnNeck/noteOps.js
@@ -7,3 +7,25 @@ const _note_dict = {
     'A':9,
     'B':10,
 }
+
+const intToNote = (noteInt, accidentals='#') => {
+    if(0 < noteInt || noteInt > 12){
+        notesSharps = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        notesFlats = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+
+        if(accidentals == '#'){
+            return notesSharps[noteInt]
+        } else if(accidentals == 'b'){
+            return notesFlats[noteInt]
+        } else {
+            //raise FormatError()
+            console.log(`${accidentals} is not valid as accidental.`);
+            
+        }
+    } else {
+        // raise RangeError()
+        console.log(`int is out of bounds (0-11): ${noteInt}`);
+    }
+
+}
+
