330f4e75cb789e96627bb32c61433c5b772aded0
unknown <keegood8@gmail.com>
2020-01-17 19:15:58 -0800

    create profile.html template, setup signal in Profile model to create Profile for each user on save. append urls to find media.

--- shreddit.io/shreddit.io/shreddit/urls.py
+++ shreddit.io/shreddit.io/shreddit/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -27,4 +29,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('', include('pages.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
