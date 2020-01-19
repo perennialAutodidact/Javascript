e06c3fb46c60c6965c585a86a536bc750691fb02
unknown <keegood8@gmail.com>
2020-01-19 00:43:58 -0800

    add mobile nav menu and content cover, disable scrolling on menu toggle, css and js

--- shreddit.io/shreddit.io/static/js/mobile-nav.js
+++ shreddit.io/shreddit.io/static/js/mobile-nav.js
@@ -1,13 +1,23 @@
 let burger = document.querySelector('.hamburger'),
     topBun = document.querySelector('.top-bun'),
     meat = document.querySelector('.meat'),
-    bottomBun = document.querySelector('.bottom-bun');
+    bottomBun = document.querySelector('.bottom-bun'),
+    cover = document.querySelector('.content-cover'),
+    menu = document.querySelector('.mobile-nav-menu');
+    body = document.querySelector('body');
 
-    console.log("hello world");
 burger.addEventListener('click', () => {
+    body.classList.toggle('no-scroll');
 
-    
+    burger.classList.toggle('hamburger-active');
+
     topBun.classList.toggle('top-bun-active');
     meat.classList.toggle('meat-active');
     bottomBun.classList.toggle('bottom-bun-active');
+
+    cover.classList.toggle('content-cover-active');
+
+    menu.classList.toggle('mobile-menu-active');
+
 });
+
